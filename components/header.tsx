8d532a8269f289611a65ba99cbf0819de439a230
mbasargan <mehmet@vollstack.com.tr>
2022-07-07 17:21:30 +0300

    day 1 complate

---
+++ components/header.tsx
@@ -0,0 +1,46 @@
+import { BellIcon, SearchIcon } from "@heroicons/react/solid";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+const Header = () => {
+    /* Sticky Header */
+    const [isScrolled, setIsScrolled] = useState(false);
+    useEffect(() => {
+        const handleScrol = () => {
+            if (window.scrollY > 0) {
+                setIsScrolled(true);
+            } else {
+                setIsScrolled(false);
+            }
+        };
+        window.addEventListener("scroll", handleScrol);
+        return () => {
+            window.removeEventListener("scroll", handleScrol);
+        };
+    }, []);
+
+    return (
+        <header className={`${isScrolled && "bg-[#141414]"}`}>
+            <div className="flex items-center space-x-2 md:space-x-10">
+                <img src="https://rb.gy/ulxxee" width={100} height={100} className="cursor-pointer object-contain" />
+                <ul className="hidden space-x-4 md:flex">
+                    <li className="header-link">Home</li>
+                    <li className="header-link">TV Shows</li>
+                    <li className="header-link">Movies</li>
+                    <li className="header-link">New & Popular</li>
+                    <li className="header-link">My List</li>
+                </ul>
+            </div>
+            <div className="flex items-center space-x-4 text-sm font-light">
+                <SearchIcon className="hidden w-6 h-6 sm:inline" />
+                <p className="hidden lg:inline">Kids</p>
+                <BellIcon className="w-6 h-6" />
+                <Link href="/account">
+                    <img src="https://rb.gy/g1pwyx" alt="" className="cursor-pointer rounded" />
+                </Link>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
